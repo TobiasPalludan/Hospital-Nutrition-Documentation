f5e6067b0736b1b4d858e6e281d288df30eb056c
SCARRESS-PC\Scarress <mjesse12@student.aau.dk>
2016-12-06 14:44:25 +0100

    Added structs too fileHandler.c

--- FileHandler.c
+++ FileHandler.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DEPARTMENT_SIZE 15
+#define NAME_SIZE 30
+#define ALLERGY_SIZE 50
+#define PREFERENCES_SIZE 100
+
+typedef struct PersonInfo {
+	int id;
+	char department[DEPARTMENT_SIZE];
+	int cprNumber;
+	char name[NAME_SIZE];
+	int height;
+	char allergy[ALLERGY_SIZE];
+	char preferences[PREFERENCES_SIZE];
+}PersonInfo;
+
+typedef struct foodIntake {
+	char *dishName;
+	char *indgredients;
+	int totalKJ;
+	int percentageEaten;
+}foodIntake;
+
 int main(void)
 {
 
